cc96066e0752ed91f1ed8678ef31a62cfe7f7a8f
Anshumaankhare2403 <anshumaan24khare1@gmail.com>
2025-01-24 23:58:03 +0530

    ak

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import tailwindcss from '@tailwindcss/vite'
+function Home() {
+  return (
+    <>
+   
+
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default Home
